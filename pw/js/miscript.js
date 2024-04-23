f3adf13630ce03c7aab0e69e2ecf5a2169a6be0f
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2024-04-23 10:59:57 -0700

    Add files via upload

---
+++ pw/js/miscript.js
@@ -0,0 +1,11 @@
+var a = 0;
+let b = 0; //scope - alcance - ambito
+const pi = 3.1416;
+alert("JS en proceso");
+function boton(){
+    alert('Clic')
+}
+
+const flecha = ()=>{
+    alert("Fucion flecha - anonima");
+}
