6cbb2752981568e50fac31b31f3e88de80fcfbef
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2024-04-25 10:52:46 -0700

    Conexion a MySQL
    Conexion a MySQL

---
+++ pw/proyecto/backend/app.js
@@ -0,0 +1,50 @@
+let express = require('express');
+let mysql = require('mysql');
+let cors = require('cors');
+
+let app = express();
+app.use(express.json());
+app.use(cors());
+
+//Conexion a MySQL
+let conexion = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//Nos conectamos a MySql
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }
+    else{
+        console.log('Conectado a la base de datos');
+    }
+});
+
+//Rutas de acceso
+app.get("/", function(req, res){
+    res.send("Ruta de Inicios");
+});
+
+//Seleccionamos todos los clientes
+app.get('/api/clientes', (req, res)=>{
+    conexion.query('SELECT * FROM clientes', (error, filas) =>{
+        if(error){
+            throw error;
+        }
+        else{
+            res.send(filas);
+        }
+    });
+
+});
+
+//Encender servidor
+let puerto = 3000;
+app.listen(puerto, function(){
+    console.log("Servidor escuchado puerto: " + puerto);
+});
