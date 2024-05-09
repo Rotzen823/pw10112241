65f12d2866fc8be09c1014140195d5638bb8ceda
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2024-05-09 10:59:27 -0700

    Pantalla Clientes
    Pantalla Clientes

---
+++ pw/proyecto/frontend/tienda/src/views/ClientesView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4>Clientes
+                    <RouterLink to="/clientes/create" class="btn btn-primary float-end">
+                        Agregar
+                    </RouterLink>
+                </h4>
+            </div>
+        </div>
+    </div>
+</template>
